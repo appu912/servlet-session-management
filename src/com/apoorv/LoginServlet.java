5587acd90eb0af1124dcbcf05de95304bcf189d3
Apoorv Mehra <mehra.appu912@gmail.com>
2024-03-18 21:29:38 +0530

    Added xml configuration

--- src/com/apoorv/LoginServlet.java
+++ src/com/apoorv/LoginServlet.java
@@ -1,20 +1,17 @@
 package com.apoorv;
 
-import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginServlet
- */
-@WebServlet(description = "This servlet handles the login logic.", urlPatterns = { "/LoginServlet" })
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println(request.getHeader("Content-Length"));
-		System.out.println(request.getMethod());
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		response.getWriter().write("<h1>Hello, from loginServletPath</h1>");
 	}
 
 }
