63e41a2f958639e535a3bf589d18fafb3092bf15
Apoorv Mehra <mehra.appu912@gmail.com>
2024-03-19 00:35:40 +0530

    query parameters

--- src/com/apoorv/LoginServlet.java
+++ src/com/apoorv/LoginServlet.java
@@ -1,6 +1,7 @@
 package com.apoorv;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +11,10 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.setContentType("text/html");
-		response.getWriter().write("<h1>Hello, from loginServletPath</h1>");
+		Map<String, String[]> parameters = request.getParameterMap();
+		parameters.forEach((x, y) -> System.out.println(x + " " + y));
+//		response.setContentType("text/html");
+//		response.getWriter().write("<h1>Hello, " + userName + " with password " + password + " from LoginServlet</h1>");
 	}
 
 }
