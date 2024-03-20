5f93c0f72a4084f67cf128615823d812b2677d9c
Apoorv Mehra <mehra.appu912@gmail.com>
2024-03-20 12:55:14 +0530

    Added welcome file

--- src/com/apoorv/LoginServlet.java
+++ src/com/apoorv/LoginServlet.java
@@ -1,7 +1,6 @@
 package com.apoorv;
 
 import java.io.IOException;
-import java.util.Map;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,10 +10,12 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		Map<String, String[]> parameters = request.getParameterMap();
-		parameters.forEach((x, y) -> System.out.println(x + " " + y));
-//		response.setContentType("text/html");
-//		response.getWriter().write("<h1>Hello, " + userName + " with password " + password + " from LoginServlet</h1>");
+		String userName = request.getParameter("username");
+		String password = request.getParameter("password");
+		System.out.println(request.getQueryString());
+		response.setContentType("text/html");
+		response.getWriter().println("username: " + userName + "\npassword: " + password);
+		response.getWriter().flush();
 	}
 
 }
